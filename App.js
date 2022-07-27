77eed4a25eaf305dc763d59ae896239edcf31539
unknown <cataniafrancoar@gmail.com>
2022-07-27 10:20:05 -0300

    Reducción

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useCallback, useEffect, useMemo, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import { Animated, StyleSheet, Text, View} from "react-native";
 
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { Colors } from './constants/styles';
-import AuthContextProvider, { AuthContext } from './store/auth-context';
-import IconButton from './components/ui/IconButton';
+import { Colors } from './src/constants/styles';
+import LoginScreen from './src/screens/LoginScreen';
+import PrincipalScreen from './src/screens/PrincipalScreen';
+import AuthContextProvider, { AuthContext } from './src/store/auth-context';
+import IconButton from './src/components/ui/IconButton';
 
+// Inicializar App y Auth
+import './src/util/auth'
+
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 const Stack = createNativeStackNavigator();
+
 
 function AuthStack() {
   return (
@@ -21,8 +32,7 @@ function AuthStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Ingreso' }}/>
     </Stack.Navigator>
   );
 }
@@ -38,8 +48,8 @@ function AuthenticatedStack() {
       }}
     >
       <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
+        name="Principal"
+        component={PrincipalScreen}
         options={{
           headerRight: ({ tintColor }) => (
             <IconButton
@@ -60,8 +70,8 @@ function Navigation() {
 
   return (
     <NavigationContainer>
-      {!authCtx.isAuthenticated && <AuthStack />}
-      {authCtx.isAuthenticated && <AuthenticatedStack />}
+      {!authCtx.email && <AuthStack />}
+      {!!authCtx.email && <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
@@ -70,9 +80,77 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <AuthContextProvider>
-        <Navigation />
-      </AuthContextProvider>
+      <AnimatedSplashScreen image={require('./assets/splash.png')}>
+        <MainScreen />
+      </AnimatedSplashScreen>
     </>
+  );
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      setTimeout(() => SplashScreen.hideAsync(), 300);
+    } catch (e) {
+
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 8,
+        duration: 700,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              // opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.manifest.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+function MainScreen() {
+  return (
+    <AuthContextProvider>
+      <Navigation />
+    </AuthContextProvider>
   );
 }
--- src/screens/PrincipalScreen.js
+++ src/screens/PrincipalScreen.js
@@ -1,274 +1,94 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ActivityIndicator, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
 
-import { Colors } from '../constants/styles';
-import { getFirestore, getDoc, doc, updateDoc, onSnapshot } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+export default function App() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
 
-
-export default function PrincipalScreen() {
-  const db = getFirestore();
-  
-  const auth = getAuth();
-  const uid = auth.currentUser.uid;
-  const userRef = doc(db, 'usuarios', uid);
-
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(true);
-  const [credito, setCredito] = useState(0);
-  const [cargando, setCargando] = useState(true);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-    
-  useEffect(() => {
-    const q = getDoc(userRef);
-
-    const unsubscribe = onSnapshot(userRef, doc => {
-      const usuario = doc.data();
-
-      const creditos = usuario.creditos;
-
-      if (creditos) {
-        let acumulador = 0;
-
-        acumulador += creditos['diez'] ? creditos['diez'] * 10 : 0;
-        acumulador += creditos['cincuenta'] ? creditos['cincuenta'] * 50 : 0;
-        acumulador += creditos['cien'] ? creditos['cien'] * 100 : 0;
-
-        setCredito(acumulador);
-      }
-      setCargando(false);
-    });
-
-    return unsubscribe;
-  }, [])
-
-  const handleBarCodeScanned = async ({ type, data }) => {
-    setScanned(true);
-    setCargando(true);
-    
-    const codigoQR = data.trim();
-
-    const userSnap = await getDoc(userRef);
-    const usuario = userSnap.data();
-
-    if (usuario.perfil !== 'admin') {
-      let clave = '';
-      
-      if (codigoQR === '8c95def646b6127282ed50454b73240300dccabc') {
-        clave = 'diez';
-      }
-      else if (codigoQR == 'ae338e4e0cbb4e4bcffaf9ce5b409feb8edd5172') {
-        clave = 'cincuenta';
-      }
-      else if (codigoQR === '2786f4877b9091dcad7f35751bfcf5d5ea712b2f') {
-        clave = 'cien';
-      }
-
-      if (clave !== '') {
-        if (!usuario.creditos || !usuario.creditos[clave]) {
-          setError('');
-
-          await updateDoc(userRef, {
-            [`creditos.${clave}`]: 1
-          });
-        }
-        else {
-          const vecesCargado = usuario.creditos[clave];
-
-          if (vecesCargado < 1 || !vecesCargado) {
-            setError('');
-
-            await updateDoc(userRef, {
-              [`creditos.${clave}`]: 1
-            });
-          }
-          else {
-            setError('Error: Código ya utilizado.');
-            setCargando(false);
-          }
-        }
-      }
-      else {
-        setError('Error: QR inválido.');
-        setCargando(false);
-      }
-    }
-    else if (usuario.perfil == 'admin') {
-      let clave = '';
-      
-      if (codigoQR === '8c95def646b6127282ed50454b73240300dccabc') {
-        clave = 'diez';
-      }
-      else if (codigoQR == 'ae338e4e0cbb4e4bcffaf9ce5b409feb8edd5172') {
-        clave = 'cincuenta';
-      }
-      else if (codigoQR === '2786f4877b9091dcad7f35751bfcf5d5ea712b2f') {
-        clave = 'cien';
-      }
-
-      if (clave !== '') {
-        if (!usuario.creditos || !usuario.creditos[clave]) {
-          setError('');
-
-          await updateDoc(userRef, {
-            [`creditos.${clave}`]: 1
-          });
-        }
-        else {
-          const cantidad = usuario.creditos[clave];
-
-          if (cantidad < 2) {
-            setError('');
-
-            await updateDoc(userRef, {
-              [`creditos.${clave}`]: cantidad + 1
-            });
-          }
-          else {
-            setError('Error: Código ya utilizado 2 veces.');
-            setCargando(false);
-          }
-        }
-      }
-      else {
-        setError('Error: QR inválido.');
-        setCargando(false);
-      }
-    }
+  const _slow = () => {
+    Gyroscope.setUpdateInterval(1000);
   };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+  const _fast = () => {
+    Gyroscope.setUpdateInterval(16);
+  };
 
-  function escanearPressHandler() {
-    setError('');
-    setScanned(false);
-  }
+  const _subscribe = () => {
+    setSubscription(
+      Gyroscope.addListener(gyroscopeData => {
+        setData(gyroscopeData);
+      })
+    );
+  };
 
-  async function anularPressHandler() {
-    setError('');
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
 
-    await updateDoc(userRef, {
-      creditos: {}
-    });
-  }
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
 
+  const { x, y, z } = data;
   return (
     <View style={styles.container}>
-
-      <View style={styles.anularContainer}>
-        <Button
-          onPress={anularPressHandler}
-          title="Anular crédito"
-          color="#FF0000"
-        />
+      <Text style={styles.text}>Gyroscope:</Text>
+      <Text style={styles.text}>
+        x: {round(x)} y: {round(y)} z: {round(z)}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+          <Text>{subscription ? 'On' : 'Off'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
       </View>
-
-      <View style={styles.creditoContainer}>
-        <Text style={styles.creditoTitulo}>
-          Tu crédito:
-        </Text>
-        {
-          cargando ?
-          <ActivityIndicator
-            size="large"
-            color="white"
-          />
-          :
-          <Text style={styles.creditoTexto}>
-            {credito}
-          </Text>
-        }
-      </View>
-
-      <View style={styles.errorContainer}>
-        <Text style={styles.errorTexto}>
-          { error }        
-        </Text>
-      </View>
-
-      <View style={styles.botonContainer}>
-        <Button
-          onPress={escanearPressHandler}
-          title="Escanear QR"
-          color={Colors.primary800}
-        >
-        </Button>
-      </View>
-
-      {
-        !scanned &&
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-          // style={StyleSheet.escaner}
-        />
-      }
-      
     </View>
   );
 }
 
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+  return Math.floor(n * 100) / 100;
+}
+
 const styles = StyleSheet.create({
-  // escaner: {
-  //   flex: 1
-  // },
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    padding: 30
+    justifyContent: 'center',
+    paddingHorizontal: 10,
   },
-  creditoContainer: {
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    // backgroundColor: '#FFF9FB',
-    backgroundColor: Colors.primary800,
-    borderRadius: 5,
-    flex: 2
+  text: {
+    textAlign: 'center',
   },
-  botonContainer: {
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
   },
-  errorContainer: {
-    // backgroundColor: Colors.error100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 0.5,
-    borderRadius: 5,
-    margin: 20
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
   },
-  anularContainer: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
-  errorTexto: {
-    color: Colors.error500,
-    fontSize: 18
-  },
-  creditoTexto: {
-    // color: Colors.primary800,
-    color: 'white',
-    fontSize: 54
-  },
-  creditoTitulo: {
-    color: Colors.primary800,
-    color: 'white',
-    fontSize: 40
-  }
-
-}); 
+});
