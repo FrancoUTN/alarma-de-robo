24f39597c0c5647cc7af1dfacc4e731c13d8b828
unknown <cataniafrancoar@gmail.com>
2022-07-28 21:50:39 -0300

    AnimatedSplashScreen extraído de App

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { useContext, useCallback, useEffect, useMemo, useState } from "react";
+import { useContext } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import Constants from "expo-constants";
-import * as SplashScreen from "expo-splash-screen";
-import { Animated, StyleSheet, Text, View} from "react-native";
 
 import { Colors } from './src/constants/styles';
 import LoginScreen from './src/screens/LoginScreen';
@@ -15,12 +12,9 @@ import ModalScreen from "./src/screens/ModalScreen";
 
 // Inicializar App y Auth
 import './src/util/auth'
+import AnimatedSplashScreen from "./src/components/inicio/AnimatedSplashScreen";
 
 
-// Instruct SplashScreen not to hide yet, we want to do this manually
-SplashScreen.preventAutoHideAsync().catch(() => {
-  /* reloading the app might trigger some race conditions, ignore them */
-});
 const Stack = createNativeStackNavigator();
 
 
@@ -94,65 +88,6 @@ export default function App() {
         <MainScreen />
       </AnimatedSplashScreen>
     </>
-  );
-}
-
-function AnimatedSplashScreen({ children, image }) {
-  const animation = useMemo(() => new Animated.Value(0), []);
-  const [isAppReady, setAppReady] = useState(false);
-  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
-
-  const onImageLoaded = useCallback(async () => {
-    try {
-      setTimeout(() => SplashScreen.hideAsync(), 300);
-    } catch (e) {
-
-    } finally {
-      setAppReady(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isAppReady) {
-      Animated.timing(animation, {
-        toValue: 1000,
-        duration: 1200,
-        useNativeDriver: true,
-      }).start(() => setAnimationComplete(true));
-    }
-  }, [isAppReady]);
-
-  return (
-    <View style={{ flex: 1 }}>
-      {isAppReady && children}
-      {!isSplashAnimationComplete && (
-        <Animated.View
-          pointerEvents="none"
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              backgroundColor: Constants.manifest.splash.backgroundColor,
-            },
-          ]}
-        >
-          <Animated.Image
-            style={{
-              width: "100%",
-              height: "100%",
-              resizeMode: Constants.manifest.splash.resizeMode || "contain",
-              transform: [
-                {
-                  translateX: animation,
-                },
-              ],
-            }}
-            source={image}
-            onLoadEnd={onImageLoaded}
-            fadeDuration={0}
-          />
-        </Animated.View>
-      )}
-    </View>
   );
 }
 
