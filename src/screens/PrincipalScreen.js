0aab1a41929e120d9cfefcdbc700515becf8946b
unknown <cataniafrancoar@gmail.com>
2022-07-29 07:06:54 -0300

    Acelerómetro

--- src/screens/PrincipalScreen.js
+++ src/screens/PrincipalScreen.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
+
 import { Colors } from '../constants/styles';
 
 
@@ -7,11 +9,88 @@ export default function Principal() {
   const [activada, setActivada] = useState(true);
   
   function onPressHandler() {
-    console.log("Apretaste.");
+    setActivada(
+      estadoActual => !estadoActual
+    );
   }
+
+
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => {
+    Gyroscope.setUpdateInterval(2000);
+  };
+
+  const _fast = () => {
+    Gyroscope.setUpdateInterval(100);
+  };
+
+  const _subscribe = () => {
+    Gyroscope.setUpdateInterval(500);
+    setSubscription(
+      Gyroscope.addListener(gyroscopeData => {
+
+
+        setData(gyroscopeData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    const valorAbsoluto = Math.abs(data.x);
+
+    if (valorAbsoluto > 5) {
+      // console.log("X superó " + valorAbsoluto + "con: " + data.x);
+      console.log("X superó 5 con: " + data.x);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   _subscribe();
+  //   return () => _unsubscribe();
+  // }, []);
+
+  const { x, y, z } = data;
+
+  function round(n) {
+    if (!n) {
+      return 0;
+    }
+    return Math.floor(n * 100) / 100;
+  }
+
 
   return (
     <View style={styles.container}>
+
+
+      <View style={styles.container}>
+        <Text style={styles.text}>Gyroscope:</Text>
+        <Text style={styles.text}>
+          x: {round(x)} y: {round(y)} z: {round(z)}
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+            <Text>{subscription ? 'On' : 'Off'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+            <Text>Slow</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={_fast} style={styles.button}>
+            <Text>Fast</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
 
       <Pressable
         style={styles.pressable}
@@ -52,7 +131,29 @@ const styles = StyleSheet.create({
     borderRadius: 5
   },
   imagen: {
-    flex: 0.8,
-    color: 'white'
-  }
+    flex: 0.8
+  },
+
+  text: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+
+
 });
